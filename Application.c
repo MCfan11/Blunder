d4ef0de1a86fa4f10ed62c60fe2072472143fb0e
Illya <illya.rachynskyy@campus.tu-berlin.de>
2025-12-25 16:20:31 +0100

    Utilizing Win32 to create a popup window (compile with MSVC from now on)

--- Application.c
+++ Application.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <Windows.h>
 
-int main()
+// cl Application.c /link user32.lib "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual Studio 2022\Visual Studio Tools\Developer Command Prompt for VS 2022.lnk"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	printf("Somebody once told me...");
+	MessageBoxW(NULL, L"Somebody once told me...", L"Info", MB_OK);
+	return 0;
 }
